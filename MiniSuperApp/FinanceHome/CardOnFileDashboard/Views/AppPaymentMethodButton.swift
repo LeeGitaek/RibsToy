bb0c4eeb563780272ea7e46b1b5ea2aa4d63024c
gitaeklee@doinglab.com <coolfamily77@gmail.com>
2022-02-09 21:24:32 +0900

    Initial Commit

---
+++ MiniSuperApp/FinanceHome/CardOnFileDashboard/Views/AppPaymentMethodButton.swift
@@ -0,0 +1,43 @@
+//
+//  AppPaymentMethodButton.swift
+//  MiniSuperApp
+//
+//  Created by gitaeklee on 2022/02/09.
+//
+
+import UIKit
+
+final class AppPaymentMethodButton: UIControl {
+    private let plusIcon: UIImageView = {
+        let imageView = UIImageView(
+            image: UIImage(
+                systemName: "plus",
+                withConfiguration: UIImage.SymbolConfiguration(pointSize: 24, weight: .semibold)
+            )
+        )
+        imageView.tintColor = .white
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return imageView
+    }()
+    
+    init() {
+        super.init(frame: .zero)
+        setupViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupViews()
+    }
+    
+    private func setupViews() {
+        addSubview(plusIcon)
+        
+        NSLayoutConstraint.activate([
+            plusIcon.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            plusIcon.centerYAnchor.constraint(equalTo: self.centerYAnchor)
+        ])
+    }
+    
+}
---
+++ MiniSuperApp/FinanceHome/FinanceHomeBuilder.swift
@@ -0,0 +1,56 @@
+import ModernRIBs
+
+protocol FinanceHomeDependency: Dependency {
+  // TODO: Declare the set of dependencies required by this RIB, but cannot be
+  // created by this RIB.
+}
+
+final class FinanceHomeComponent: Component<FinanceHomeDependency>,
+                                    SuperPayDashboardDependency,
+                                    CardOnFileDashboardDependency {
+    var balance: ReadOnlyCurrentValuePublisher<Double>{ balancePublisher }
+    private let balancePublisher: CurrentValuePublisher<Double>
+    
+    init(
+        dependency: FinanceHomeDependency,
+        balance: CurrentValuePublisher<Double>
+    ) {
+        self.balancePublisher = balance
+        super.init(dependency: dependency)
+    }
+  // TODO: Declare 'fileprivate' dependencies that are only used by this RIB.
+}
+
+// MARK: - Builder
+
+protocol FinanceHomeBuildable: Buildable {
+  func build(withListener listener: FinanceHomeListener) -> FinanceHomeRouting
+}
+
+final class FinanceHomeBuilder: Builder<FinanceHomeDependency>, FinanceHomeBuildable {
+  
+  override init(dependency: FinanceHomeDependency) {
+    super.init(dependency: dependency)
+  }
+  
+  func build(withListener listener: FinanceHomeListener) -> FinanceHomeRouting {
+    let balancePublisher = CurrentValuePublisher<Double>(10000000)
+    
+    let component = FinanceHomeComponent(
+        dependency: dependency,
+        balance: balancePublisher
+    )
+    let viewController = FinanceHomeViewController()
+    let interactor = FinanceHomeInteractor(presenter: viewController)
+    interactor.listener = listener
+      
+    let superPayDashboardBuilder = SuperPayDashboardBuilder(dependency: component)
+    let cardOnFileDashboardBuilder = CardOnFileDashboardBuilder(dependency: component)
+    return FinanceHomeRouter(interactor: interactor,
+                             viewController: viewController,
+                             superPayDashboardBuildable: superPayDashboardBuilder,
+                             cardOnFileDashboardBuildable: cardOnFileDashboardBuilder
+                             
+    )
+  }
+}
